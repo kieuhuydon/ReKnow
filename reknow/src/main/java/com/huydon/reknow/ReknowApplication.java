8b01776ae19cbc3ac83ac7b43ec4f994f68192c4
Đôn Kiều <kieuhuydon@gmail.com>
2026-04-02 18:16:08 +0700

    init:set up project

---
+++ reknow/src/main/java/com/huydon/reknow/ReknowApplication.java
@@ -0,0 +1,13 @@
+package com.huydon.reknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReknowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReknowApplication.class, args);
+	}
+
+}
